83063374c360911159ac7ae7a4f53864d27f423d
nepuni <ojambo04@gmail.com>
2019-08-09 14:29:53 +0530

    Upload Attendance file status

---
+++ routers/api/attendance/functions/countUploadedFiles.js
@@ -0,0 +1,76 @@
+async function countUploadedFile(a_directory, a_file_prefix, a_filename_length, a_file_extension) {
+  
+    let p = new Promise((resolve, reject) => {
+      if (
+        (
+          (a_directory !== '')        &&
+          (a_directory !== null)      &&
+          (a_directory !== undefined) &&
+          (a_directory !== false)
+        ) &&
+        (
+          (a_file_prefix !== '')        &&
+          (a_file_prefix !== null)      &&
+          (a_file_prefix !== undefined) &&
+          (a_file_prefix !== false)
+        ) &&
+        (
+          (a_filename_length !== '')        &&
+          (a_filename_length !== null)      &&
+          (a_filename_length !== undefined) &&
+          (a_filename_length !== false)     &&
+          (a_filename_length !== 0)
+        ) &&
+        (
+          (a_file_extension !== '')        &&
+          (a_file_extension !== null)      &&
+          (a_file_extension !== undefined) &&
+          (a_file_extension !== false)
+        )
+      ) {
+        const fs = require('fs');
+        
+        directory        = a_directory;
+        file_prefix      = a_file_prefix;
+        file_name_length = a_filename_length;
+        file_extension   = a_file_extension;
+        
+        fs.readdir(directory, 'utf-8', (error, files) => {
+          console.log('Count uploaded files : ' + directory);
+          if (!error) {
+            let attendance_file = [];
+            let machine_ids     = [];
+            
+            for (let file of files) {
+              if (
+                (file.length                   === file_name_length) &&
+                (file.startsWith(file_prefix)  === true) &&
+                (
+                  file.endsWith(file_extension) === true || 
+                  file.endsWith(file_extension.toLowerCase()) === true ||
+                  file.endsWith(file_extension.toUpperCase()) === true
+                  )
+              ) {
+                attendance_file.push(file);
+              } else {
+                continue;
+              }
+            }
+            
+            for (let file of attendance_file) {
+              machine_ids.push(file.substring(6, 9));
+            }
+            resolve({status: true, result: machine_ids});
+          } else {
+            reject({status: false, result: 'Can not read the upload directory'});
+          }
+        });
+      } else {
+        reject({status: false, result: 'Issue with function parameter'});
+      }
+    });
+           
+    return p;
+  }
+
+  module.exports = countUploadedFile
--- routers/api/attendance/uploadAttendanceData.router.js
+++ routers/api/attendance/uploadAttendanceData.router.js
@@ -1,14 +1,16 @@
-const router = require("express").Router({mergeParams: true});
+const router = require("express").Router({ mergeParams: true });
 const path = require("path");
 const insertIntoPunchingRec = require("./functions/insertIntoPunchingRec");
 const PunchingRecModel = require('../../../model/attendance/punchingRec.model')
+const countUploadedFile = require('./functions/countUploadedFiles');
+const enumerateBetweenDates = require('./functions/enumerateDaysBetweenDates');
 
 const multer = require("multer");
 const storage = multer.diskStorage({
-  destination: function(req, file, cb) {
+  destination: function (req, file, cb) {
     cb(null, "./uploads/");
   },
-  filename: function(req, file, cb) {
+  filename: function (req, file, cb) {
     let name = file.originalname;
     cb(null, name);
   }
@@ -16,7 +18,7 @@ const storage = multer.diskStorage({
 
 const upload = multer({
   storage: storage,
-  fileFilter: function(req, file, cb) {
+  fileFilter: function (req, file, cb) {
     if (
       !(
         path.extname(file.originalname) === ".dat" ||
@@ -54,9 +56,9 @@ router.route("/").post((req, res) => {
         return PunchingRecModel.bulkCreate(resultArr, {
           updateOnDuplicate: ["punching_date", "emp_code", "punching_time"]
         })
-        .then(() => {
-          res.status(200).json({ message: "Successfully uploaded the dat file" });
-        })
+          .then(() => {
+            res.status(200).json({ message: "Successfully uploaded the dat file" });
+          })
       })
       .catch(err => {
         console.log(err);
@@ -67,6 +69,44 @@ router.route("/").post((req, res) => {
   });
 });
 
-router.route("/upload-status").get((req, res) => {});
+router.route("/status").get(async (req, res) => {
+  const fromDate = req.query.from_date;
+  const toDate = req.query.to_date;
+
+  let dates = enumerateBetweenDates(fromDate, toDate);
+  console.log(dates);
+  let upload_status = [];
+
+  try {
+    for (let date of dates) {
+      let day = date.getDate();
+      let month = date.getMonth() + 1;
+      let year = date.getFullYear();
+      let year_number = '';
+      day = day < 10 ? '0' + day.toString() : day.toString();
+      month = month < 10 ? '0' + month.toString() : month.toString();
+      year_number = (year.toString()).substring(2, 4);
+      file_prefix = day + month + year_number;
+      console.log(date);
+      console.log(file_prefix);
+      let result = await countUploadedFile("C:\\Users\\it neepco\\hrms-server\\uploads", file_prefix, 13, '.DAT');
+      console.log(result);
+      if (result.status !== false) {
+        if (result.result.length > 0) {
+          upload_status.push({
+            'punch_day': year.toString() + '-' + month + '-' + day,
+            machine_ids: result.result
+          });
+        }
+      }
+    }
+    res.status(200).json({ status: upload_status });
+  } catch (e) {
+    console.log(err);
+    return res
+      .status(500)
+      .json({ message: "oops error occured", error: err });
+  }
+});
 
 module.exports = router;
